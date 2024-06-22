81477d09ea472e4f8b0affb8b12de4b96a9930ef
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-06-22 15:24:17 -0400

    web scraping with puppeteer

---
+++ notes.js
@@ -0,0 +1,6 @@
+// Notes 1
+//   const bodyHandle = await page.$("body");
+//   const html = await page.evaluate((body) => body.innerHTML, bodyHandle);
+//   await bodyHandle.dispose();
+//   console.log(html);
+//   browser.close();
